30dd523d606738e49d68791aae93f06e2aacb37e
Harshit Rajak <106432378+harshitrajak18@users.noreply.github.com>
2024-09-28 23:24:13 +0530

    Update settings.py

--- ml/settings.py
+++ ml/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-vh7cvl@oif!s_qt&eh!!ojf3e!&7@fufa2h2h830)xie78hbxc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1','8000']
 
 
 # Application definition
