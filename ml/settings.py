9b8f972b30e46cfab515f5f0e123b7a0c8d99c71
Harshit Rajak <106432378+harshitrajak18@users.noreply.github.com>
2024-09-28 23:49:29 +0530

    Update settings.py

--- ml/settings.py
+++ ml/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+PORT = int(os.environ.get('PORT', 8000)) 
 
 
 # Quick-start development settings - unsuitable for production
